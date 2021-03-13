6d5706f07e96018d7b0558c18c05552b0cb3d657
John Stoddart <Stoddart.Johna@yahoo.com>
2021-03-13 11:24:24 -0600

    We got the flask app to get the data properly pushed to HTML

--- app.py
+++ app.py
@@ -7,14 +7,20 @@ app = Flask(__name__)
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 db = client.weather
-
 col = db.data
 
+weather_data = scrape_weather.scrape()
+from pprint import pprint
+pprint(weather_data['alabama'])
+col.remove()
+col.insert(weather_data)
 
 @app.route("/")
 def home():
     
-    data = col.find_one()
+    data = list(col.find())
+    print ("squid")
+    print (data)
     return render_template("index.html", weather=data)
 
 
@@ -22,7 +28,7 @@ def home():
 def scrape():
 
     weather_data = scrape_weather.scrape()
-    col.insert_one(weather_data)
+    col.insert(weather_data)
     print("start")
     print (list(col.find()))
     print("finish")
