b5ac8d6e24e84286ed4e84498eb4624b717c09a3
John Stoddart <Stoddart.Johna@yahoo.com>
2021-03-15 23:19:45 -0500

    Flask app is now rendering the maps properly with API calls

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify, request
 import pymongo 
 import scrape_weather
+import json
+from bson import ObjectId
+from pprint import pprint
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 app = Flask(__name__)
 
@@ -9,19 +18,15 @@ client = pymongo.MongoClient(conn)
 db = client.weather
 col = db.data
 
-weather_data = scrape_weather.scrape()
-from pprint import pprint
-pprint(weather_data['alabama'])
-col.remove()
-col.insert(weather_data)
+# weather_data = scrape_weather.scrape()
+# from pprint import pprint
+# pprint(weather_data['alabama'])
+# col.remove()
+# col.insert(weather_data)
 
 @app.route("/")
 def home():
-    
-    data = list(col.find())
-    print ("squid")
-    print (data)
-    return render_template("index.html", weather=data)
+    return render_template("index.html")
 
 
 @app.route("/scrape")
@@ -29,15 +34,39 @@ def scrape():
 
     weather_data = scrape_weather.scrape()
     col.insert(weather_data)
-    print("start")
-    print (list(col.find()))
-    print("finish")
     return redirect("/")
 
-@app.route("/hightemp")
-def highmap():
-    return render_template("hightemp.html", weather=data)
+@app.route("/maps")
+def statemap():
+    return render_template("maps.html")
 
+
+@app.route('/state', methods=['GET'])
+def testfn():
+    # GET request
+    data = list(col.find())
+    if request.method == 'GET':
+        data = JSONEncoder().encode(data)
+        message = json.load(open("./static/data/statesData.geojson"))
+        return message
+
+@app.route('/master', methods=['GET'])
+def testfn2():
+    # GET request
+    data = list(col.find())
+    if request.method == 'GET':
+        data = JSONEncoder().encode(data)
+        message = json.load(open("./static/data/masterdata.geojson"))
+        return message
+
+@app.route('/complete', methods=['GET'])
+def testfn3():
+    # GET request
+    data = list(col.find())
+    if request.method == 'GET':
+        data = JSONEncoder().encode(data)
+        message = json.load(open("./static/data/complete_us.geojson"))
+        return message
 
 if __name__ == "__main__":
     app.run(debug=True)
